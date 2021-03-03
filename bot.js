e8cfeffcb6459f12b46ca3083d73acbacc996cd4
Douglas Araujo <dodopokcamilo@gmail.com>
2021-03-02 23:37:08 -0300

    feat: poder pingar o bot pra ter certeza que ainda tá rodando

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 const TelegramBot = require('node-telegram-bot-api');
 const token = process.env.TELEGRAM_BOT_KEY;
-const bot = new TelegramBot(token, {polling: false});
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+  bot.sendMessage(process.env.TELEGRAM_CHAT_ID, "Ainda tô aqui!");
+});
 
 module.exports = {
   log: function (message) {
